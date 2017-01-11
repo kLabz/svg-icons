6605dfb31f0a35bf4b3b6ad9a0059635a4efddf7
Rudy Ges <rudy.geslin@gmail.com>
2017-01-11 10:40:58 +0100

    Fixed indentation

--- src/svgicons/macro/SVGSprite.hx
+++ src/svgicons/macro/SVGSprite.hx
@@ -11,85 +11,85 @@ using StringTools;
 
 class SVGSprite {
 	public static function buildSVGSprite():Array<Field> {
-	var fields = Context.getBuildFields();
+		var fields = Context.getBuildFields();
 
-	var path = Compiler.getDefine("SVG_PATH");
-	if (path == null) path = "../img";
-	path = Context.resolvePath(path);
+		var path = Compiler.getDefine("SVG_PATH");
+		if (path == null) path = "../img";
+		path = Context.resolvePath(path);
 
-	if (FileSystem.exists(path)) {
-		var iconsMap:Dynamic = {};
-		var icons = FileSystem.readDirectory(path);
-		var hasDefaultIcon:Bool = false;
-
-		#if SVG_SPRITE_OUTPUT
-		var spriteOutput:String = '<svg xmlns="http://www.w3.org/2000/svg">' +
-		'<defs><style>svg svg { display: none; } svg svg:target { display: inline; }</style></defs>';
-
-		var svgIdReg = ~/^\s*<svg(.*)(\sid=("|')[a-zA-Z0-9_-]+("|'))/;
-
-		#end
-
-		for (icon in icons) {
-			var iconName = icon.replace(".svg", "");
-			var iconPath = path + "/" + icon;
+		if (FileSystem.exists(path)) {
+			var iconsMap:Dynamic = {};
+			var icons = FileSystem.readDirectory(path);
+			var hasDefaultIcon:Bool = false;
 
 			#if SVG_SPRITE_OUTPUT
-				var isBG:Bool = iconName.endsWith("-bg");
+			var spriteOutput:String = '<svg xmlns="http://www.w3.org/2000/svg">' +
+			'<defs><style>svg svg { display: none; } svg svg:target { display: inline; }</style></defs>';
+
+			var svgIdReg = ~/^\s*<svg(.*)(\sid=("|')[a-zA-Z0-9_-]+("|'))/;
+
 			#end
 
-			#if USE_SVGO
-				var configPath = Context.resolvePath("../svgo.yml");
-				var process = new Process("./node_modules/svgo/bin/svgo", ["-i", iconPath, "-o", "-", "--config", configPath]);
-				var iconData = process.stdout.readLine();
-			#else
-				var iconData = File.getContent(path + "/" + icon);
-			#end
+			for (icon in icons) {
+				var iconName = icon.replace(".svg", "");
+				var iconPath = path + "/" + icon;
 
-			#if SVG_SPRITE_OUTPUT
-				if (isBG) {
-					if (svgIdReg.match(iconData)) {
-						iconData = svgIdReg.replace(iconData, '<svg$1');
+				#if SVG_SPRITE_OUTPUT
+					var isBG:Bool = iconName.endsWith("-bg");
+				#end
+
+				#if USE_SVGO
+					var configPath = Context.resolvePath("../svgo.yml");
+					var process = new Process("./node_modules/svgo/bin/svgo", ["-i", iconPath, "-o", "-", "--config", configPath]);
+					var iconData = process.stdout.readLine();
+				#else
+					var iconData = File.getContent(path + "/" + icon);
+				#end
+
+				#if SVG_SPRITE_OUTPUT
+					if (isBG) {
+						if (svgIdReg.match(iconData)) {
+							iconData = svgIdReg.replace(iconData, '<svg$1');
+						}
+
+						iconData = iconData.replace('<svg ', '<svg id="' + iconName + '" ');
+						spriteOutput += iconData;
+					} else {
+				#end
+						if (iconName == "default") hasDefaultIcon = true;
+						Reflect.setField(iconsMap, iconName, iconData);
+				#if SVG_SPRITE_OUTPUT
 					}
+				#end
+			}
 
-					iconData = iconData.replace('<svg ', '<svg id="' + iconName + '" ');
-					spriteOutput += iconData;
-				} else {
-			#end
-					if (iconName == "default") hasDefaultIcon = true;
-					Reflect.setField(iconsMap, iconName, iconData);
 			#if SVG_SPRITE_OUTPUT
-				}
+				spriteOutput += '</svg>';
+
+				var spritePath = Context.resolvePath(Compiler.getDefine("SVG_SPRITE_OUTPUT")) + "svg-backgrounds.svg";
+				var file = File.write(spritePath);
+				file.writeString(spriteOutput);
+				file.close();
 			#end
+
+			fields.push({
+				pos: Context.currentPos(),
+				name: "hasDefaultIcon",
+				meta: null,
+				kind: FieldType.FVar(macro: Bool, macro $v{hasDefaultIcon}),
+				doc: null,
+				access: [Access.APrivate, Access.AStatic]
+			});
+
+			fields.push({
+				pos: Context.currentPos(),
+				name: "icons",
+				meta: null,
+				kind: FieldType.FVar(macro: Dynamic<String>, macro $v{iconsMap}),
+				doc: null,
+				access: [Access.APrivate, Access.AStatic]
+			});
 		}
-
-		#if SVG_SPRITE_OUTPUT
-			spriteOutput += '</svg>';
-
-			var spritePath = Context.resolvePath(Compiler.getDefine("SVG_SPRITE_OUTPUT")) + "svg-backgrounds.svg";
-			var file = File.write(spritePath);
-			file.writeString(spriteOutput);
-			file.close();
-		#end
-
-		fields.push({
-			pos: Context.currentPos(),
-			name: "hasDefaultIcon",
-			meta: null,
-			kind: FieldType.FVar(macro: Bool, macro $v{hasDefaultIcon}),
-			doc: null,
-			access: [Access.APrivate, Access.AStatic]
-		});
-
-		fields.push({
-			pos: Context.currentPos(),
-			name: "icons",
-			meta: null,
-			kind: FieldType.FVar(macro: Dynamic, macro $v{iconsMap}),
-			doc: null,
-			access: [Access.APrivate, Access.AStatic]
-		});
-	}
 
 		return fields;
 	}
